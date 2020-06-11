180d6bd762e97865e501e1805517491b166c72fe
eltoulemonde <toul@octo.com>
2020-06-11 10:30:52 +0200

    Remove sleep

--- formation_indus_ds_avancee/data_loading.py
+++ formation_indus_ds_avancee/data_loading.py
@@ -10,9 +10,8 @@ def get_data_from_csv():
     df = pd.read_csv(df_path, sep=';')
     data1 = df.sample(n=1, random_state=1)
     data1.Date_time = datetime.datetime.now()
-    sleep(60)
     data2 = df.sample(n=1, random_state=1)
-    data2.Date_time = datetime.datetime.now()
+    data2.Date_time = datetime.datetime.now() + datetime.timedelta(seconds=60)
     new_data = data1.concate(data2)
     new_data.to_csv('../../input/'+'data_'+str(datetime.datetime.now())+'.csv')
 
