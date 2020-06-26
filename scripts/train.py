89fd94974a1ccce34b5bce28bfb90231c3b161c5
eltoulemonde <toul@octo.com>
2020-06-26 14:45:41 +0200

    clean master

--- scripts/train.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-from formation_indus_ds_avancee.feature_engineering import prepare_features
-from formation_indus_ds_avancee.train_and_predict import train_model
-from config import TRAIN_DATA_PATH, MODEL_REGISTRY_FOLDER
-
-train_df = pd.read_csv(TRAIN_DATA_PATH, sep=';')
-prepared_features_df = prepare_features(train_df, training_mode=True)
-train_model(prepared_features_df, MODEL_REGISTRY_FOLDER)
--- tests/test_functional/features/training_workflow.feature
+++
@@ -1,7 +0,0 @@
-Feature: Model training
-  I want to be able to train by model when a dataset containing the features and the target is available.
-
-  Scenario: train the model when training data is available
-     Given training data is available
-      When I launch the training
-      Then a model is added to the model registry
