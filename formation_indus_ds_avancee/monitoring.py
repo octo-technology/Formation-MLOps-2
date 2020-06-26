a2ea37afdbe9b9b7129ac2414a3c4b715f7b0171
eltoulemonde <toul@octo.com>
2020-06-26 12:08:08 +0200

    Prepare tp

--- formation_indus_ds_avancee/monitoring.py
+++ formation_indus_ds_avancee/monitoring.py
@@ -20,5 +20,7 @@ def monitor_with_io(predictions_folder: str, db_con_str: str, monitoring_table_n
 
 
 def monitor(latest_predictions: pd.DataFrame) -> pd.DataFrame:
-    monitoring_df = latest_predictions.groupby('predictions_time').agg({'predictions': 'mean'}).reset_index()
+    # Start filling function
+
+    # End filling function
     return monitoring_df
--- tests/test_unit/test_monitoring.py
+++ tests/test_unit/test_monitoring.py
@@ -2,12 +2,14 @@ import os
 from unittest.mock import patch
 
 import pandas as pd
+from pytest import mark
 from sqlalchemy import create_engine
 
 from dags.config import MONITORING_TABLE_NAME
 from formation_indus_ds_avancee.monitoring import monitor_with_io
 
 
+@mark.skip("Complete monitor function and test, then remove the mark.skip")
 @patch('pandas.read_csv')
 def test_monitor_with_io_should_write_predictions_mean_to_db(mocked_read_csv):
     # Given
@@ -17,7 +19,9 @@ def test_monitor_with_io_should_write_predictions_mean_to_db(mocked_read_csv):
                                 'predictions': [12, 14]})
     mocked_read_csv.return_value = predictions
     db_con_str = 'sqlite:///test_db.db'
-    expected = pd.DataFrame({'predictions_time': [given_date], 'predictions': [13]})
+    # Start filling expected
+    expected = ...
+    # End filling expected
 
     # When
     monitor_with_io(predictions_folder, db_con_str, monitoring_table_name=MONITORING_TABLE_NAME)
@@ -29,3 +33,8 @@ def test_monitor_with_io_should_write_predictions_mean_to_db(mocked_read_csv):
     pd.testing.assert_frame_equal(expected, actual)
     db_conn.close()
     os.remove('test_db.db')
+
+
+def test_monitoring_table_name_as_been_changed():
+    # Please change monitoring table name so that you don't overwrite monitoring from other attendees
+    assert MONITORING_TABLE_NAME != "monitoring"
