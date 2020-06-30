e8f6aca9bc7cf26663141573f09a9d49ecd02f11
eltoulemonde <toul@octo.com>
2020-06-30 16:16:52 +0200

    Prepare tp

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -18,8 +18,7 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     y = features[target]
     model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
     model.fit(X, y)
-    time_str = time.strftime('%Y%m%d-%H%M%S')
-    joblib.dump(model, os.path.join(model_registry_folder, time_str + '.joblib'))
+    joblib.dump(model, os.path.join(model_registry_folder, 'model.joblib'))
 
 
 def predict_with_io(features_path: str, model_path: str, predictions_folder: str) -> None:
