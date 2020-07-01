1e6a97fe3d26a348e8d530f7a6e834c6d16f15b7
eltoulemonde <toul@octo.com>
2020-07-01 17:00:48 +0200

    Fix typo

--- formation_indus_ds_avancee/data_loading.py
+++ formation_indus_ds_avancee/data_loading.py
@@ -10,5 +10,5 @@ def get_data_from_csv(train_data_path: str, data_folder: str) -> None:
     random_line_2 = df.sample(n=1, random_state=1)
     random_line_2.Date_time = datetime.datetime.now() + datetime.timedelta(seconds=60)
     new_data = pd.concat([random_line_1, random_line_2], ignore_index=True)
-    new_data.to_csv(os.path.join(data_folder + str(datetime.datetime.now()) + '.csv'), index=False)
+    new_data.to_csv(os.path.join(data_folder, str(datetime.datetime.now()) + '.csv'), index=False)
     # TODO to change, should point to a generated data folder which would then be used for predictions
