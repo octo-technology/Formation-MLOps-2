e9b56b0a7bf09f0d6ca139717a5ec32830e4c618
eltoulemonde <toul@octo.com>
2021-06-17 10:26:35 +0200

    Change start_date

--- dags/predict.py
+++ dags/predict.py
@@ -2,6 +2,8 @@ import os
 import sys
 from datetime import timedelta, datetime
 
+from airflow.utils.dates import days_ago
+
 sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))  # So that airflow can find config files
 
 from airflow.decorators import dag, task
@@ -14,7 +16,7 @@ from dags.config import GENERATED_DATA_PATH, DATA_FOLDER, MODEL_PATH, PREDICTION
 from formation_indus_ds_avancee.feature_engineering import prepare_features_with_io
 
 
-@dag(default_args={'owner': 'airflow'}, schedule_interval=timedelta(minutes=2), start_date=datetime.now())
+@dag(default_args={'owner': 'airflow'}, schedule_interval=timedelta(minutes=2), start_date=days_ago(n=0, hour=1))
 def predict():
     @task
     def prepare_features_with_io_task():
