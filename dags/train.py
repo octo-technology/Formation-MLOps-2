26b7a8e0f3f2dffb39a69fd80b44f47783632191
eltoulemonde <toul@octo.com>
2021-12-15 16:23:51 +0100

    REFACTO: Add flake8 & vulture to CI, then refacto

--- dags/train.py
+++ dags/train.py
@@ -2,34 +2,30 @@ import os
 import sys
 from datetime import timedelta
 
-sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))  # So that airflow can find config files
-
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
 
-from dags.config import TRAIN_DATA_PATH, FEATURES_PATH, MODEL_REGISTRY_FOLDER
+from dags.config import TRAIN_DATA_PATH, MODEL_REGISTRY_FOLDER, DATA_FOLDER
 from formation_indus_ds_avancee.feature_engineering import prepare_features_with_io
 from formation_indus_ds_avancee.train_and_predict import train_model_with_io
 
-dag = DAG(dag_id='train',
-          description='Training DAG',
-          start_date=days_ago(1),
-          schedule_interval=timedelta(weeks=4))
+sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))  # So that airflow can find config files
 
-prepare_features = PythonOperator(task_id='prepare_features',
-                                  python_callable=prepare_features_with_io,
-                                  dag=dag,
-                                  provide_context=False,
-                                  op_kwargs={'data_path': TRAIN_DATA_PATH,
-                                             'features_path': FEATURES_PATH,
-                                             'training_mode': True})
 
-train_model = PythonOperator(task_id='train_model',
-                             python_callable=train_model_with_io,
-                             dag=dag,
-                             provide_context=False,
-                             op_kwargs={'features_path': FEATURES_PATH,
-                                        'model_registry_folder': MODEL_REGISTRY_FOLDER})
+@dag(default_args={'owner': 'airflow'}, schedule_interval=timedelta(weeks=4), start_date=days_ago(1))
+def train_model():
+    @task
+    def prepare_features_task() -> str:
+        feature_train_path: str = os.path.join(DATA_FOLDER, 'prepared_features_train.parquet')
+        prepare_features_with_io(data_path=TRAIN_DATA_PATH, features_path=feature_train_path, training_mode=True)
+        return feature_train_path
 
-prepare_features >> train_model
+    @task
+    def train_model_task(feature_path: str) -> None:
+        train_model_with_io(features_path=feature_path, model_registry_folder=MODEL_REGISTRY_FOLDER)
+
+    feature_path = prepare_features_task()
+    train_model_task(feature_path)
+
+
+train_model_dag = train_model()
