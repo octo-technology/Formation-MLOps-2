8b9c0282520d7b149a49394f5d2fe3c083a991fe
Ismaël Lachheb <ismael.lachheb@protonmail.com>
2022-12-09 18:00:33 +0100

    fix: issue with dags

--- dags/predict.py
+++ dags/predict.py
@@ -5,10 +5,10 @@ from datetime import timedelta, datetime
 from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
 
+sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))  # So that airflow can find config files
 from dags.config import GENERATED_DATA_PATH, DATA_FOLDER
 from formation_indus_ds_avancee.feature_engineering import prepare_features_with_io
 
-sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))  # So that airflow can find config files
 
 
 @dag(default_args={'owner': 'airflow'}, schedule_interval=timedelta(minutes=2), start_date=days_ago(n=0, hour=1))
