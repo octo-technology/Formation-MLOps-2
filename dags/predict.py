a68d5ff80ed24dc3daef0e7ee042cc398842e8f8
TOUL <toul>
2023-01-19 13:59:48 +0100

    Update versions and fix code

--- dags/predict.py
+++ dags/predict.py
@@ -2,17 +2,19 @@ import os
 import sys
 from datetime import timedelta, datetime
 
+import pendulum
 from airflow.decorators import dag, task
-from airflow.utils.dates import days_ago
-
-from dags.config import GENERATED_DATA_PATH, DATA_FOLDER, MODEL_PATH, PREDICTIONS_FOLDER
-from formation_indus_ds_avancee.feature_engineering import prepare_features_with_io
-from formation_indus_ds_avancee.train_and_predict import predict_with_io
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))  # So that airflow can find config files
 
+from dags.config import GENERATED_DATA_PATH, DATA_FOLDER, MODEL_PATH, PREDICTIONS_FOLDER, MONITORING_TABLE_NAME
+from formation_indus_ds_avancee.feature_engineering import prepare_features_with_io
+from formation_indus_ds_avancee.train_and_predict import predict_with_io
 
-@dag(default_args={'owner': 'airflow'}, schedule_interval=timedelta(minutes=2), start_date=days_ago(n=0, hour=1))
+
+
+@dag(default_args={'owner': 'airflow'}, schedule=timedelta(minutes=2),
+     start_date=pendulum.today('UTC').add(hours=-1))
 def predict():
     @task
     def prepare_features_with_io_task():
