2de42a0d31af5343298da4258f5629768b705cbd
Théophile Molcard <molcard.t@gmail.com>
2023-02-10 09:25:46 +0100

    Fix : training dag imports (#1)

--- dags/train.py
+++ dags/train.py
@@ -5,11 +5,11 @@ from datetime import timedelta
 from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
 
+sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))  # So that airflow can find config files
+
 from dags.config import TRAIN_DATA_PATH, MODEL_REGISTRY_FOLDER, DATA_FOLDER
 from formation_indus_ds_avancee.feature_engineering import prepare_features_with_io
 from formation_indus_ds_avancee.train_and_predict import train_model_with_io
-
-sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))  # So that airflow can find config files
 
 
 @dag(default_args={'owner': 'airflow'}, schedule_interval=timedelta(weeks=4), start_date=days_ago(1))
