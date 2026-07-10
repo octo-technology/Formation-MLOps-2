61c06d951e57b998aa9e8cbbaed82f2bc19a037a
el-toulemonde <toul@octo.com>
2026-07-10 13:21:24 +0200

    Update

--- dags/config.py
+++ dags/config.py
@@ -13,6 +13,6 @@ GENERATED_DATA_PATH = os.path.join(DATA_FOLDER, 'la-haute-borne-data-2017-2020.c
 FEATURES_PATH = os.path.join(DATA_FOLDER, 'prepared_features.parquet')
 
 MODEL_REGISTRY_FOLDER = os.path.join(PROJECT_FOLDER, 'models')
-MODEL_PATH = os.path.join(MODEL_REGISTRY_FOLDER, '20200616-140746.joblib')  # To change when needed
+MODEL_PATH = os.path.join(MODEL_REGISTRY_FOLDER, '20260710-112838.joblib')  # To change when needed
 
 MONITORING_TABLE_NAME = 'monitoring'
