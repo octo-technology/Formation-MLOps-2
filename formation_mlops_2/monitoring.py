8b34eb85b80b3dd638d1f673190cb1ac23479c4c
el-toulemonde <toul@octo.com>
2026-07-15 15:11:21 +0200

    Fix ci

--- formation_mlops_2/monitoring.py
+++ formation_mlops_2/monitoring.py
@@ -3,6 +3,9 @@ import pandas as pd
 
 def monitor(latest_predictions: pd.DataFrame) -> pd.DataFrame:
     # Start filling function
-    monitoring_df = pd.DataFrame
+    monitoring_df = pd.DataFrame({
+        "predictions_time": [None],
+        "predictions": [None]
+    })
     # End filling function
     return monitoring_df
