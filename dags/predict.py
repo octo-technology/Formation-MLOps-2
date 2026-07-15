9ab10a13e7b4f197bf97e818cf59c6eb1f864e5a
el-toulemonde <toul@octo.com>
2026-07-15 15:00:23 +0200

    Wip fix ci

--- dags/predict.py
+++ dags/predict.py
@@ -6,9 +6,9 @@ from airflow.sdk import dag, task
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))  # So that airflow can find config files
 
-from dags.config import DATA_FOLDER, GENERATED_DATA_PATH, MODEL_PATH, PREDICTIONS_FOLDER
+from dags.config import DATA_FOLDER, GENERATED_DATA_PATH
 from formation_mlops_2.feature_engineering_io import prepare_features_with_io
-from formation_mlops_2.train_and_predict_io import predict_with_io
+from formation_mlops_2.train_and_predict_io import predict_with_io  # noqa
 
 
 # Here we use catchup=False, due to TP contexte, in other contexte either use catchup=True,
@@ -23,13 +23,15 @@ def predict():
                                  features_path=features_path,
                                  training_mode=False)
         return features_path
+
     # Start completing predict task
     def predict_with_io_task():
         pass
+
     # End completing predict task
 
-    feature_path = prepare_features_with_io_task()
-    # predict_with_io_task(feature_path=feature_path)
+    # feature_path = prepare_features_with_io_task() # noqa
+    # predict_with_io_task(feature_path=feature_path) # noqa
 
 
 predict_dag = predict()
