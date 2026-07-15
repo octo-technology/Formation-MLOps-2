5b99f0fac588f45500f70107aaf294ef92767905
el-toulemonde <toul@octo.com>
2026-07-15 14:34:53 +0200

    Init tp 5

--- formation_mlops_2/train_and_predict.py
+++ formation_mlops_2/train_and_predict.py
@@ -1,8 +1,6 @@
 import os
-import time
 
 import joblib
-import mlflow
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 
@@ -11,18 +9,9 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     target = 'Ba_avg'
     df_x = features.drop(columns=[target])
     y = features[target]
-    with mlflow.start_run():
-        # log_models set to False because it doesn't work so we will log manually
-        mlflow.sklearn.autolog(log_models=False)
-        model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
-        model.fit(df_x, y)
-        mlflow.sklearn.log_model(
-            sk_model=model,
-            name="sklearn_model",
-            registered_model_name="sklearn_model",
-        )
-    time_str = time.strftime('%Y%m%d-%H%M%S')
-    joblib.dump(model, os.path.join(model_registry_folder, time_str + '.joblib'))
+    model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
+    model.fit(df_x, y)
+    joblib.dump(model, os.path.join(model_registry_folder, 'model.joblib'))
 
 
 def predict(features: pd.DataFrame, model_path: str) -> pd.DataFrame:
