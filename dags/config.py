2b0d1e9c6bb154b2f2125ecd452e096da531a22f
el-toulemonde <toul@octo.com>
2026-07-23 13:24:18 +0200

    FIX

--- dags/config.py
+++ dags/config.py
@@ -9,7 +9,7 @@ PREDICTIONS_FOLDER = os.path.join(DATA_FOLDER, 'predictions')
 TRAIN_DATA_PATH = os.path.join(DATA_FOLDER, 'la-haute-borne-data-2017-2020.csv')
 GENERATED_DATA_PATH = os.path.join(DATA_FOLDER, 'latest.csv')
 
-FEATURES_PATH = os.path.join(DATA_FOLDER, 'prepared_features.parquet')
+FEATURES_PATH = os.path.join(GENERATED_DATA_FOLDER, 'prepared_features.parquet')
 
 MODEL_REGISTRY_FOLDER = os.path.join(PROJECT_FOLDER, 'models')
 MODEL_PATH = os.path.join(MODEL_REGISTRY_FOLDER, '20260710-112838.joblib')  # To change when needed
